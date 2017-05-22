45c16b9f7e7307ce48e147f91cffbfe08bf99437
Pedro Victor <pedro.victor.ferreira@outlook.com.br>
2017-05-22 19:08:45 -0400

    Basic Meteors Behavior add

---
+++ Assets/Scripts/IReadyStart.cs
@@ -0,0 +1,6 @@
+﻿
+public interface IReadyStart {
+
+    void StartGame();
+
+}
---
+++ Assets/Scripts/MeteorsManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorsManager : MonoBehaviour , IReadyStart {
+
+    public Transform meteorRef;
+    public float radiusSpawn;
+    public Vector3 center = Vector3.zero;
+
+    public float timeRepeating = 3f;
+
+    private void InstantiateMeteor(){
+
+        Vector3 pos = RandomCircle(center, radiusSpawn);
+        Instantiate(meteorRef, pos, Quaternion.identity);
+
+    }
+
+    Vector3 RandomCircle(Vector3 center, float radius)
+    {
+        float ang = UnityEngine.Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.z = center.z;
+        return pos;
+    }
+
+    public void StartGame()
+    {
+        InvokeRepeating("InstantiateMeteor", timeRepeating, timeRepeating);
+    }
+}
