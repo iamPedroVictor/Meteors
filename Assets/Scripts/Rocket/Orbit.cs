7e888ef6b01e79a640293fad8af44ef10db9f610
Pedro Victor <pedro.victor.ferreira@outlook.com.br>
2017-05-29 17:24:25 -0400

    Rocket orbit

---
+++ Assets/Scripts/Rocket/Orbit.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpaceShipMode
+{
+    Orbit,
+    Launch
+}
+
+public class Orbit : MonoBehaviour {
+
+    public Transform centerOrbit;
+    [Range(20,100)]
+    public float speedRotateAround;
+    [Range(0, 20)]
+    public float speedLaunch;
+
+    public SpaceShipMode spaceShip;
+
+
+    public void Start()
+    {
+        spaceShip = SpaceShipMode.Orbit;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (spaceShip == SpaceShipMode.Orbit)
+            {
+                spaceShip = SpaceShipMode.Launch;
+                CallDestroy();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if(SpaceShipMode.Orbit == spaceShip)
+        {
+            OrbitAround();
+        }else
+        {
+            Launch();
+        }
+	}
+
+    private void OrbitAround()
+    {
+        transform.RotateAround(centerOrbit.position, Vector3.forward, speedRotateAround * Time.fixedDeltaTime);
+    }
+
+    private void Launch(){
+        transform.Translate(Vector3.up * (speedLaunch * Time.fixedDeltaTime));
+    }
+
+    private void CallDestroy()
+    {
+        Invoke("DestroyMe", 2f);
+    }
+
+    void DestroyMe()
+    {
+        Destroy(this.gameObject);
+        RocketsManager.Instance.Build();
+    }
+
+}
---
+++ Assets/Scripts/Rocket/RocketsManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketsManager : MonoBehaviour {
+
+    public Vector3[] rocketsOrbitPosition;
+    public GameObject rocketsBase;
+    public GameObject[] rockts;
+
+
+    private static RocketsManager _instance;
+
+    private static int count;
+
+    public static RocketsManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<RocketsManager>();
+            }
+            return _instance;
+        }
+    }
+
+
+    public void Build()
+    {
+        count++;
+
+        if (count > 1)
+        {
+            count = 0;
+            BuildRockets();
+        }
+    }
+
+
+    private void BuildRockets()
+    {
+        print("Construi Rockets");
+        for(int i = 0; i < rockts.Length; i++)
+        {
+            if (!rockts[i].activeInHierarchy){
+                rockts[i] = Instantiate(rocketsBase, rocketsOrbitPosition[i], Quaternion.Euler(0,180 * i,-90)) as GameObject;
+            }
+        }
+    }
+
+}
